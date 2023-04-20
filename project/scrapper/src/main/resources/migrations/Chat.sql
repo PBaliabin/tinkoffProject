4d29abfc89c2c8f7ac767322c58308b5971f6920
pbaliabin <baliabinpavel@bk.ru>
2023-04-20 16:42:50 +0700

    lesson5 attempt3

--- project/scrapper/src/main/resources/migrations/Chat.sql
+++ project/scrapper/src/main/resources/migrations/Chat.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS chat
 (
-    chat_id varchar(50) NOT NULL,
+    chat_id varchar(50),
     CONSTRAINT chat_pkey PRIMARY KEY (chat_id)
 )
--- project/scrapper/src/main/resources/migrations/Link.sql
+++ project/scrapper/src/main/resources/migrations/Link.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS link
 (
-    link varchar(200) NOT NULL,
+    link varchar(200),
     CONSTRAINT link_pkey PRIMARY KEY (link)
 )
