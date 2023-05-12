cb2c83ce29ac3c08d967c3397620021a7788c216
pbaliabin <baliabinpavel@bk.ru>
2023-05-12 15:59:22 +0700

    test workflow32

--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/dto/exception/NotFoundException.java
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/dto/exception/NotFoundException.java
@@ -1,15 +1,4 @@
 package ru.tinkoff.edu.java.scrapper.dto.exception;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import ru.tinkoff.edu.java.scrapper.dto.response.ApiErrorResponse;
-
-@EqualsAndHashCode(callSuper = true)
-@Data
-@RequiredArgsConstructor
-@NoArgsConstructor
 public class NotFoundException extends RuntimeException {
-    public ApiErrorResponse apiErrorResponse;
 }
